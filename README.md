fa7df27f56a0c86c0017939ad4a1b2680930194b
Mark Wolfe <mark@wolfe.id.au>
2014-11-20 14:20:53 +1100

    Initial version.

---
+++ README.md
@@ -0,0 +1,23 @@
+# simpleinfluxdb
+
+[![NPM](https://nodei.co/npm/svcs.png)](https://nodei.co/npm/simpleinfluxdb/)
+[![NPM](https://nodei.co/npm-dl/svcs.png)](https://nodei.co/npm/simpleinfluxdb/)
+
+This library aims to provide a simple interface to add and read data from [influxdb](http://influxdb.com).
+
+# Status
+
+Don't use it in production, early adopters and hackers are welcome, the API is still in a state of flux as we work on
+evolving some of the core features.
+
+# API
+
+
+```javascript
+
+```
+
+# TODO
+
+## License
+Copyright (c) 2013 Mark Wolfe released under the MIT license.
---
+++ test/simpleinfluxdb_test.js
@@ -0,0 +1,125 @@
+'use strict';
+
+var debug = require('debug')('test:simpleinfluxdb');
+var SimpleInfluxDB = require('../index');
+var expect = require('chai').expect;
+
+describe('simpleinfluxdb', function () {
+
+    var sdb = new SimpleInfluxDB();
+
+    it('simpleinfluxdb should load defaults', function () {
+
+
+        debug('simpleinfluxdb', sdb);
+
+        // check the defaults.
+        expect(sdb.config.hostname).to.equal('localhost');
+        expect(sdb.config.database).to.equal('mydb');
+        expect(sdb.config.username).to.equal('root');
+        expect(sdb.config.password).to.equal('root');
+
+    });
+
+    it('should remap period names', function (done) {
+
+        sdb._remapPeriod('1day', function (err, res) {
+            expect(err).to.not.exist;
+            expect(res).to.equal('1d');
+            done();
+        })
+
+    });
+
+    it('should read and raise a function error', function (done) {
+        var sdbread = new SimpleInfluxDB();
+
+        SimpleInfluxDB.prototype._callApi = function (method, path, body, cb) {
+            cb(null, {})
+        };
+
+        sdbread.read(new Date().getTime(), new Date().getTime(), {'function': 'woot'}, function (err, data) {
+            expect(err).to.match(/function must be one of/);
+            done();
+        })
+
+    });
+
+    it('should read and raise a number error if start or end is a string', function (done) {
+        var sdbread = new SimpleInfluxDB();
+
+        SimpleInfluxDB.prototype._callApi = function (method, path, body, cb) {
+            cb(null, {})
+        };
+
+        sdbread.read(new Date().getTime(), new Date().getTime(), {
+            'function': 'count',
+            start: 'hello',
+            end: 'foo'
+        }, function (err, data) {
+            expect(err).to.match(/start must be a number/);
+            sdbread.read(new Date().getTime(), new Date().getTime(), {
+                'function': 'count',
+                start: new Date().getTime(),
+                end: 'foo'
+            }, function (err, data) {
+                expect(err).to.match(/end must be a number/);
+                done();
+            })
+        })
+
+    });
+
+    it('should write and return an array error', function (done) {
+        var sdbread = new SimpleInfluxDB();
+
+        SimpleInfluxDB.prototype._callApi = function (method, path, body, cb) {
+            cb(null, {})
+        };
+
+        sdbread.write_key('some_key', {}, function (err, res) {
+            expect(err).to.match(/value must be an array/);
+            done();
+        })
+    });
+
+    it('should write and return a date error', function (done) {
+        var sdbread = new SimpleInfluxDB();
+
+        var testPath = "";
+
+        SimpleInfluxDB.prototype._callApi = function (method, path, body, cb) {
+            testPath = path;
+            cb(null, {})
+        };
+
+        sdbread.write_key('some_key', [{t: 'woot', v: 123.1}], function (err, res) {
+            expect(err).to.match(/value position 0 fails because t must be a number of milliseconds or valid date string/);
+            expect(testPath).to.equal("");
+            done();
+        })
+    });
+
+    it('should write and return a valid path', function (done) {
+        var sdbread = new SimpleInfluxDB();
+
+        var testPath = "";
+        var testBody = {};
+
+        SimpleInfluxDB.prototype._callApi = function (method, path, body, cb) {
+            testPath = path;
+            testBody = body;
+            cb(null, {})
+        };
+
+        sdbread.write_key('some_key', [{t: new Date(1416451118000), v: 123.1}], function (err, res) {
+            expect(testPath).to.equal("/series?u=root&p=root");
+            expect(testBody.name).to.equal('some_key');
+            expect(testBody.columns).to.deep.equal(['time', 'value']);
+            expect(testBody.points).to.deep.equal(
+                [[1416451118000, 123.1]]
+            );
+            done();
+        })
+    });
+});
